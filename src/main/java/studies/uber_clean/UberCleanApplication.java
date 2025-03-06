aeed68d728ade88aff405b3c76a0ec3810f5b293
Kamilek <radek71k@onet.pl>
2025-03-06 08:48:27 +0100

    spring init

---
+++ src/main/java/studies/uber_clean/UberCleanApplication.java
@@ -0,0 +1,13 @@
+package studies.uber_clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studies/uber_clean/UberCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package studies.uber_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
